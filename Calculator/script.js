23c5247472c1e1fff7b1f257a42b30f60c92bbf5
DXstudios1 <122834463+DXstudios1@users.noreply.github.com>
2024-12-12 14:56:50 -0300

    Update script.js

--- Calculator/script.js
+++ Calculator/script.js
@@ -3,6 +3,7 @@ function AddNumber(value) {
     document.getElementById("CalculatorValue").textContent = ""
   }
   document.getElementById("CalculatorValue").textContent += String(value)
+  document.getElementById("PreviousCalculatorValue").textContent = "???"
 }
 
 function Href(value) {
@@ -11,21 +12,28 @@ function Href(value) {
 
 function Clear() {
   document.getElementById("CalculatorValue").textContent = "?"
+  document.getElementById("PreviousCalculatorValue").textContent = "???"
 }
 
 function Delete() {
   var CurrentText = document.getElementById("CalculatorValue").textContent
+  document.getElementById("PreviousCalculatorValue").textContent = "???"
   document.getElementById("CalculatorValue").textContent = CurrentText.slice(0, -1)
+  CurrentText = document.getElementById("CalculatorValue").textContent
+  if (CurrentText == "") {
+    document.getElementById("CalculatorValue").textContent = "?"
+  }
 }
 
 function Result() {
   var CurrentString = String(document.getElementById("CalculatorValue").textContent)
+  document.getElementById("PreviousCalculatorValue").textContent = CurrentString + "="
   document.getElementById("CalculatorValue").textContent = eval(CurrentString)
 }
 
 // KEY
 document.addEventListener("keyup", function(key){
-  if (key.key == "1" || key.key == "2" || key.key == "3" || key.key == "4" || key.key == "5" || key.key == "6" || key.key == "7" || key.key == "8" || key.key == "9" || key.key == "0" || key.key == "+" || key.key == "-" || key.key == "*" || key.key == "/" || key.key == ".") {
+  if (key.key == "1" || key.key == "2" || key.key == "3" || key.key == "4" || key.key == "5" || key.key == "6" || key.key == "7" || key.key == "8" || key.key == "9" || key.key == "0" || key.key == "+" || key.key == "-" || key.key == "*" || key.key == "/" || key.key == "." || key.key == "%") {
     AddNumber(key.key)
   }
 })
